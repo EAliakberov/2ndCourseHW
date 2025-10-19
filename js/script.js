9bcecdc1f7dd44321f3085625d128065234e76d6
Eugene Aliakberov <eugenealiakberov@gmail.com>
2025-10-19 12:57:44 +0300

    Do HomeWork6

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let btnPlayNumbers = document.getElementById('btn-play-numbers');
+let btnPlayCalc = document.getElementById('btn-play-calc');
 
 let gameNumbers = {
     randNum: 0,
@@ -30,11 +31,31 @@ let gameNumbers = {
         }
         alert((this.randNum === this.lastAns ? 'Вы ' : 'Вы не ') + `угадали: загаданное число: ${this.randNum}; ваше число: ${this.lastAns}`);
     }
+
+    
 }
 
-
+function playCalc() {
+    const tasks = [{ sym: '+', funk: (a, b) => a + b },
+    { sym: '-', funk: (a, b) => a - b },
+    { sym: '*', funk: (a, b) => a * b },
+    { sym: '/', funk: (a, b) => a / b }];
+    for (let i = 0; i < 20; i++) {
+        a = Math.floor(Math.random() * 100 + 1);
+        b = Math.floor(Math.random() * 100 + 1);
+        let type = Math.floor(Math.random() * 4);
+        let res = tasks[type].funk(a, b);
+        let userAns = prompt(`Вычислите выражение ${a} ${tasks[type].sym} ${b}.\nДля выхода введите не число`);
+        if (isNaN(userAns)) break;
+        alert(Math.abs(+userAns - res) < 0.01 ? `Верно, правильный ответ ${res}` : `Неверно, правильный ответ ${res}`);
+    }
+}
 
 btnPlayNumbers.addEventListener('click', (ev) => {
     gameNumbers.startGame();
+})
+
+btnPlayCalc.addEventListener('click', (ev) => {
+    playCalc();
 })
 
